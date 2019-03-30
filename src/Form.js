1527a94ea108f1114f574f6bd12f79b3d116d7c4
Mary E Ellis <mary.e.ellis@gmail.com>
2019-03-30 01:50:02 -0400

    beginnings of error handling but messy

--- src/Form.js
+++ src/Form.js
@@ -7,12 +7,14 @@ class Search extends Component {
         super();
         this.state = {
             zip: "10036",
-            weather: ["Scattered Thundershowers"],
+            description: ["Scattered Thundershowers"],
             name: "New York",
             temp: "16",
             high: "22",
             low: "6",
+            icon: "10d",
             isLoading: false,
+            error: null,
         }
     }
     handleInputChange = (e) => {
@@ -26,51 +28,87 @@ class Search extends Component {
           fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${this.state.zip},us&units=imperial&appid=709847967f5e54b97308c1b2cae4dee5`)
           .then(response => response.json())
           .then(data => {
-            console.log(data)
-            let weather = data.weather.map((weather) => {
-              return(
-                <span key={weather.response}>{weather.description}</span>
-              )
-            })
+              console.log(data)
+              if(data.cod === "404"){
+                  console.log('yippee!')
+                    this.setState({
+                        isLoaded: true,
+                        error: true,
+                    });
+            } else {
             this.setState({
-              name: data.name,
-              weather: weather,
-              temp: data.main.temp,
-              high: data.main.temp_max,
-              low: data.main.temp_min,
-            })
-          })
-        }
-        else {
-      
-        }
-      }
+                name: data.name,
+                description: data.weather[0].description,
+                temp: Math.round(data.main.temp),
+                high: Math.round(data.main.temp_max),
+                low: Math.round(data.main.temp_min),
+                icon: data.weather[0].icon,
+                error: null,
+            });
+          }
+    })
+    }    
+    }
 
   render() {
+    const { error } = this.state;
+    if (error) {
+        return(
+             <div>
+                 <span>Whoops! Try another zip :)</span>
+                 <div className="form">
+                <form onSubmit={this.handleInputChange} noValidate> 
+                <label>Zip Code:</label>
+                    <input
+                        id="zip" 
+                        name="zip" 
+                        type="text" 
+                        ppattern="^\s*?\d{5}(?:[-\s]\d{4})?\s*?$"
+                        placeholder="10036"
+                        ref={input => this.search = input}
+                        onChange={(e) => this.setState({ str: e.target.value })}
+                        required
+                    />
+                    <button type='submit'>Update</button>
+                
+                </form>
+            </div>
+        </div>
+        )
+    } else {
     return (
         <div>
             <div className="response">
-                <p className="city-name">{this.state.name}</p>
-                <p className="description">{this.state.weather}</p>
+                <p className="city-name">{this.state.name}
+                    <span className="icon">
+                        <img src={"http://openweathermap.org/img/w/" + this.state.icon + ".png"} alt={this.state.description + " icon"} />
+                    </span>
+                </p>
+                <p className="description">{this.state.description}</p>
                 <p className="current-temp">{this.state.temp}</p>
                 <p className="low">{this.state.low}</p>
                 <p className="high">{this.state.high}</p>
             </div>
             <div className="form">
-                Form to input new name and weather:
-                <form onSubmit={this.handleInputChange}> 
+                <form onSubmit={this.handleInputChange} noValidate> 
+                <label>Zip Code:</label>
                     <input
-                    placeholder="10036"
-                    ref={input => this.search = input}
-                    onChange={(e) => this.setState({ str: e.target.value })}
+                        id="zip" 
+                        name="zip" 
+                        type="text" 
+                        ppattern="^\s*?\d{5}(?:[-\s]\d{4})?\s*?$"
+                        placeholder="10036"
+                        ref={input => this.search = input}
+                        onChange={(e) => this.setState({ str: e.target.value })}
+                        required
                     />
                     <button type='submit'>Update</button>
-
-
+                
                 </form>
             </div>
         </div>
-    )
+        )
+    }
   }
 }
 
